5b0ec1de35bf945c6d9063e8b81cab70f3181e6c
msavinash <msavinash1139@gmail.com>
2023-09-27 16:24:44 -0700

    Chrome extension downloads pdf from remote

--- ChromeEx/scripts/background.js
+++ ChromeEx/scripts/background.js
@@ -1,18 +1,48 @@
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    const jobDescription = message.jobDescription;
-    console.log("HI!!!!!!!!!!")
+	const jobDescription = message.jobDescription;
+	return true;
+});
+
+
+// Add an event listener to the popup button
+// document.getElementById('downloadButton').addEventListener('click', function() {
+//     // Get the job description or any other required data here
+
+//     // Assuming you have the job description in a variable named 'jobDescription'
     
-    // Use NLP libraries to extract keywords from the job description.
-    // const keywords = extractKeywords(jobDescription);
-  
-    // // Send keywords to the server for user-specific customization.
-    // const customizedResume = customizeResume(keywords);
-  
-    // // Generate a PDF from the customized resume.
-    // generatePDF(customizedResume);
-  
-    // // Implement user account management logic here.
-  
-    // sendResponse({ success: true });
-  });
-  
+//     // Fetch and generate the PDF
+// 	console.log("HERE!")
+//     fetch(`https://resumegen.onrender.com/generate_pdf`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'multipart/form-data',
+//         }
+//     })
+//     .then((response) => response.blob())
+//     .then((blob) => {
+//         const filename = 'job_description.pdf';
+//         const reader = new FileReader();
+//         reader.onload = () => {
+//             const buffer = reader.result;
+//             const blobUrl = `data:${blob.type};base64,${btoa(new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ''))}`;
+            
+//             // Trigger the download when the PDF is ready
+//             chrome.downloads.download({
+//                 url: blobUrl,
+//                 filename: filename,
+//                 saveAs: true,
+//                 conflictAction: "uniquify"
+//             }, () => {
+//                 // Handle the download completion here
+//                 console.log('PDF download complete');
+//             });
+//         };
+//         reader.readAsArrayBuffer(blob);
+//     })
+//     .catch((error) => {
+//         // Handle any errors that occur during the PDF generation
+//         console.error('Failed to generate PDF:', error);
+//     });
+// });
+
+
---
+++ ChromeEx/scripts/popup.js
@@ -0,0 +1,61 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     const downloadButton = document.getElementById("downloadButton");
+  
+//     downloadButton.addEventListener("click", function () {
+//       chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+//         const activeTab = tabs[0];
+//         const jobDescription = document.body.textContent; // Replace with the job description you want to send to the API
+  
+//         chrome.runtime.sendMessage({ jobDescription }, function (response) {
+//           if (response.success) {
+//             // Handle the success response, e.g., display a success message.
+//           } else {
+//             // Handle the error response, e.g., display an error message.
+//           }
+//         });
+//       });
+//     });
+//   });
+
+
+
+
+  document.getElementById('downloadButton').addEventListener('click', function() {
+    // Get the job description or any other required data here
+
+    // Assuming you have the job description in a variable named 'jobDescription'
+    
+    // Fetch and generate the PDF
+	console.log("HERE!")
+    fetch(`https://resumegen.onrender.com/generate_pdf`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        }
+    })
+    .then((response) => response.blob())
+    .then((blob) => {
+        const filename = 'job_description.pdf';
+        const reader = new FileReader();
+        reader.onload = () => {
+            const buffer = reader.result;
+            const blobUrl = `data:${blob.type};base64,${btoa(new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ''))}`;
+            
+            // Trigger the download when the PDF is ready
+            chrome.downloads.download({
+                url: blobUrl,
+                filename: filename,
+                saveAs: true,
+                conflictAction: "uniquify"
+            }, () => {
+                // Handle the download completion here
+                console.log('PDF download complete');
+            });
+        };
+        reader.readAsArrayBuffer(blob);
+    })
+    .catch((error) => {
+        // Handle any errors that occur during the PDF generation
+        console.error('Failed to generate PDF:', error);
+    });
+});
